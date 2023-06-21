36f7726cd1e6e6238a12fd5c4bebb2eaf1b6a0c4
Leandro Gaspari <lgaspari@thecoursekey.com>
2023-06-21 17:27:09 -0300

    feat: setup PWA and Service Worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      includeAssets: ['apple-touch-icon.png', 'favicon.ico'],
+      manifest: {
+        name: 'APEX Mapy',
+        short_name: 'APEX Mapy',
+        theme_color: '#DA292A',
+        description:
+          'Progressive Web Application (PWA) that allows users to subscribe to map change notifications.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'vite.svg',
+            type: 'image/svg+xml',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 });
