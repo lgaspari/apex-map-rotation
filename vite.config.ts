ab97b434acb210168cc28bb76de554e15c2ae61f
Leandro Gaspari <lgaspari@thecoursekey.com>
2023-06-22 18:19:12 -0300

    feat: configure HTTPS for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-//vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/apex-map-rotation/' : '/',
   plugins: [tsconfigPaths(), svgr(), react()],
+  server: {
+    https: {
+      cert: fs.readFileSync('certs/cert.pem'),
+      key: fs.readFileSync('certs/key.pem'),
+    },
+    host: true,
+  },
 }));
