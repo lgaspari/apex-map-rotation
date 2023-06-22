3637165ab4300a75979a6c2d90301ab99a45be62
Leandro Gaspari <lgaspari@thecoursekey.com>
2023-06-22 18:19:28 -0300

    feat: configure Service Worker to display notifications in background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,100 @@
+interface SyncManager {
+  getTags(): Promise<string[]>;
+  register(tag: string): Promise<void>;
+}
+
+declare global {
+  interface ServiceWorkerRegistration {
+    readonly sync: SyncManager;
+  }
+
+  interface SyncEvent extends ExtendableEvent {
+    readonly lastChance: boolean;
+    readonly tag: string;
+  }
+
+  interface ServiceWorkerGlobalScopeEventMap {
+    sync: SyncEvent;
+  }
+}
+
+function registerBackgroundSync() {
+  if (!navigator.serviceWorker) {
+    return console.error('Service Worker not supported');
+  }
+
+  navigator.serviceWorker.ready
+    .then((registration) => registration.sync.register('fetch_map_rotation'))
+    .then(() => console.log('Registered background sync'))
+    .catch((err) => {
+      console.log(err);
+      console.error('Error registering background sync', err);
+    });
+}
+
+function requestNotificationPermission() {
+  if (!('Notification' in window)) {
+    // Check if the browser supports notifications
+    alert('This browser does not support desktop notification');
+  } else if (Notification.permission === 'granted') {
+    return Promise.resolve();
+  } else if (Notification.permission !== 'denied') {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === 'granted') {
+        return Promise.resolve();
+        // …
+      }
+
+      return Promise.reject('Permission was not granted.');
+    });
+  }
+}
+
+// function requestSyncManagerPermission() {
+//   if (!('SyncManager' in window)) {
+//     // Check if the browser supports notifications
+//     alert('This browser does not support background sync');
+//   } else if (Notification.permission === 'granted') {
+//     return Promise.resolve();
+//   } else if (Notification.permission !== 'denied') {
+//     // We need to ask the user for permission
+//     Notification.requestPermission().then((permission) => {
+//       // If the user accepts, let's create a notification
+//       if (permission === 'granted') {
+//         return Promise.resolve();
+//         // …
+//       }
+//       return Promise.reject('Permission was not granted.');
+//     });
+//   }
+// }
+
 function App() {
   console.log('Environment information:', import.meta.env);
-  return <div>Hello world!</div>;
+
+  const handleClick = async () => {
+    try {
+      await Promise.all([
+        requestNotificationPermission(),
+        // requestBackgroundSyncPermission(),
+      ]);
+
+      registerBackgroundSync();
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <button onClick={handleClick}>
+        Notify me when there are new attendees
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/sw.ts
+++ src/sw.ts
@@ -9,3 +9,51 @@ clientsClaim();
 cleanupOutdatedCaches();
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+// -------------------------------------------------
+
+const json = {
+  current: {
+    start: 1687393800,
+    end: 1687399200,
+    readableDate_start: '2023-06-22 00:30:00',
+    readableDate_end: '2023-06-22 02:00:00',
+    map: 'Broken Moon',
+    code: 'broken_moon_rotation',
+    DurationInSecs: 5400,
+    DurationInMinutes: 90,
+    asset: 'https://apexlegendsstatus.com/assets/maps/Broken_Moon.png',
+    remainingSecs: 2617,
+    remainingMins: 44,
+    remainingTimer: '00:43:37',
+  },
+  next: {
+    start: 1687399200,
+    end: 1687404600,
+    readableDate_start: '2023-06-22 02:00:00',
+    readableDate_end: '2023-06-22 03:30:00',
+    map: "World's Edge",
+    code: 'worlds_edge_rotation',
+    DurationInSecs: 5400,
+    DurationInMinutes: 90,
+  },
+};
+
+self.addEventListener('sync', function (event) {
+  console.log('sync event', event);
+  switch (event.tag) {
+    case 'syncAttendees': {
+      self.registration.showNotification('Test', { body: 'Body' });
+      break;
+    }
+
+    case 'fetch_map_rotation': {
+      setTimeout(() => {
+        self.registration.showNotification('Map Rotation', {
+          body: `${json.next.map} in ${json.current.remainingTimer}`,
+        });
+      }, 5000);
+      break;
+    }
+  }
+});
