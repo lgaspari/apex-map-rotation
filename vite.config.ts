4b690d7300c90bf5eb125a89013b069c04c64b81
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2023-06-26 14:07:47 -0300

    feat: setup gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+//vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/apex-map-rotation/' : '/',
   plugins: [tsconfigPaths(), react()],
-});
+}));
